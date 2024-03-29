611b56cd52548575825892a74b50dd62c18c6826
Jason Warner <xocomil@gmail.com>
2024-03-28 20:08:32 -0600

    added our footer and cleaned up the home page

--- angular-blog/src/app/app.component.ts
+++ angular-blog/src/app/app.component.ts
@@ -1,17 +1,20 @@
+import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
+import BlogFooter from './components/footer/blog-footer.component.analog';
+import BlogHeader from './components/header/blog-header.component.analog';
 import { ThemeStore } from './data-access/theme.store';
 
 @Component({
   selector: 'angular-blog-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [BlogFooter, BlogHeader, RouterOutlet, JsonPipe],
   template: `
-    <blog-header class="flex-none" />
+    <BlogHeader class="flex-none" />
     <div class="flex-grow overflow-hidden">
       <router-outlet></router-outlet>
     </div>
+    <BlogFooter class="flex-none" />
   `,
   host: {
     class: 'block flex flex-col h-full w-full max-w-full max-h-full',
